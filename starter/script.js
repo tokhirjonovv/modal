9ce2c0bd04476be9829b1f3b4207b1120bdc5678
tokhirjonovv <134261461+tokhirjonovv@users.noreply.github.com>
2023-05-25 10:43:32 +0500

    Add files via upload

---
+++ starter/script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const btnCloseModal = document.querySelector('.close-modal')
+const btnsOpenModal = document.querySelectorAll('.show-modal')
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+    btnsOpenModal[i].addEventListener('click', function
+        () {
+        console.log('Button clicked');
+        modal.classList.remove('hidden')
+        overlay.classList.remove('hidden')
+    });
+    
+
+    const closeModal =  function (){
+
+        modal.classList.add('hidden')
+        overlay.classList.add('hidden')
+    } 
+
+
+    btnCloseModal.addEventListener('click', closeModal);
+
+    overlay.addEventListener('click', closeModal);
+
+
+
+
+        document.addEventListener('keydown', function(e) {
+            console.log(e.key)
+
+            if (e.key === 'Escape') {
+                if(!modal.classList.contains('hiddeen')){
+                    closeModal();
+                }
+            }
+        });
